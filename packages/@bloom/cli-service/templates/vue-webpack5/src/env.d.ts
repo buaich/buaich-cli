935623425852c5ae51b8711e27cc92da787448c7
bloom <2597466845@qq.com>
2026-04-08 12:58:46 +0800

    bug: 修复命名问题

--- packages/@bloom/cli-service/templates/vue-webpack5/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="webpack-env" />
-
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
